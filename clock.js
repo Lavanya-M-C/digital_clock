3928e825cc7dab4916ca642da7d9e17394645a55
root <mc.lavanya@mahindra.com>
2020-05-06 12:07:37 +0530

    small change

--- clock.js
+++ clock.js
@@ -3,7 +3,7 @@ tick();
 setInterval(tick,1000);
 function tick(){
     let clock = document.getElementById('clock');
-    let current_time = new Date().toLocaleTimeString();
+    let current_time = new Date().toLocaleTimeString("en-US", {timeZone: "Asia/Kolkata"});
     clock.innerHTML = current_time;
 }
 
